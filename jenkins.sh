5f7a5478be31e37d7c701416fcbe4f9bb2e46cc8
HEMANT KUMAR SINGH <108391556+hemantkumarsingh114@users.noreply.github.com>
2024-07-27 22:22:46 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
-# USE UBUNTU20.04 - INSTANCE: 2GB RAM + 2VCPU MIN - WILL ONLY WORK
+
+# 1. Update Package->
 sudo apt update -y
-sudo apt install openjdk-11-jdk -y
-sudo apt update -y
+
+# 2. Install Java-11->
+sudo apt install openjdk-17-jdk -y
+
+# 3. Install Maven->
 sudo apt install maven -y
+
+# 4. Jenkins Key->
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+# 5. Jenkins List->
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# 6. Again Update->
 sudo apt update -y
+
+# 7. Install Jenkins->
 sudo apt install jenkins -y
+
+# 8. Start Jenkins->
 service jenkins start
+
+# 9. Generate Initial Password->
 cat /var/lib/jenkins/secrets/initialAdminPassword
-#chmod 777 jenkins.sh
-#./jenkins.sh
+
+# 10.chmod 777 jenkins.sh
+# 11. sh jenkins.sh
